2ee0358e3c6a1c6626bc654b839fef441ca1314b
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-11-30 13:08:33 +0530

    Database

---
+++ Pro2B/db.mjs
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"; 
+const connetDB = async() => { 
+try{
+     await   mongoose.connect('mongodb+srv://<username>:<password>@cluster0.xxxxxx.mongodb.net/TestDB') // replace with your own connection string
+console.log('conneted to Database.....') 
+}catch(error)
+{ 
+      console.log(error)
+ }
+ }
+ export default connetDB
