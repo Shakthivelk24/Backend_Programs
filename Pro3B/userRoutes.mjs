173e8f0deb0986ad052fb1005d38780374c22269
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-12-01 20:54:59 +0530

    Router

---
+++ Pro3B/userRoutes.mjs
@@ -0,0 +1,75 @@
+import express from "express";
+import userModel from "./newUserModel.mjs";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import authorize from "./newAuth.mjs";
+
+const router = express.Router();
+
+// Register
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      username,
+      email,
+      password: hashPassword
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "Registration successful..." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await userModel.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ message: "user not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.json({ message: "invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role }, //payload for token user id and role
+      "shakthivelk",
+      { expiresIn: "20m" }
+    );
+
+    res.json({ token });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+
+router.get(
+  "/",
+  authorize(["user"]),  // Protect this route for 'user' role only
+  async (req, res) => {
+    const user = await userModel
+      .findById(req.user.id)
+      .select("-password");
+
+    res.json({ user ,message:"Verified User Access Granted"});
+  }
+);
+
+export default router;
+
