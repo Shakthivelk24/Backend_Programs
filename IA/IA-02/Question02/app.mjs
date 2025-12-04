a7fe7ca2055cfb620fb6124252cb335f68cbc07b
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-12-04 12:24:16 +0530

    Server

---
+++ IA/IA-02/Question02/app.mjs
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./db.mjs";
+import userRoutes from "./userRoutes.mjs";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// DB
+connectDB();
+
+// Routes
+app.use('/api/users', userRoutes);
+
+app.listen(4000, () => console.log(`Server running at http://localhost:${4000}`));
