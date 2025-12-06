751923f2ca1840f5c57c2186ea2107e3b9d445f1
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-12-06 14:41:56 +0530

    Database

--- Pro2B/db.mjs
+++ Pro2B/db.mjs
@@ -1,11 +1,12 @@
-import mongoose from "mongoose"; 
-const connetDB = async() => { 
-try{
-     await   mongoose.connect('mongodb+srv://<username>:<password>@cluster0.xxxxxx.mongodb.net/TestDB') // replace with your own connection string
-console.log('conneted to Database.....') 
-}catch(error)
-{ 
-      console.log(error)
- }
- }
- export default connetDB
+import mongoose from "mongoose";
+const connetDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://<username>:<password>@cluster0.xxxxxx.mongodb.net/TestDB"
+    ); // replace with your own connection string
+    console.log("conneted to Database.....");
+  } catch (error) {
+    console.log(error);
+  }
+};
+export default connetDB;
