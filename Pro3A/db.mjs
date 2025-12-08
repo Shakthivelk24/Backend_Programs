8d8d2190775a8f210fc24c4c4aadaa31bd73e113
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-12-08 23:03:51 +0530

    Database

--- Pro3A/db.mjs
+++ Pro3A/db.mjs
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(
       "mongodb+srv://<username>:<password>@cluster0.xxxxxx.mongodb.net/TestDB"
-    );
+    ); // Replace ur own connection string
 
     console.log("Connected to Database...");
   } catch (error) {
