55365eb19807217e31a97e882f11fc77d0aba3b3
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-02 21:22:48 +0530

    Router for Retrive the data from the database

---
+++ SemPrograms/E-Commerce/itemRouter.mjs
@@ -0,0 +1,33 @@
+import express from "express";
+import itemModel from "./items.mjs";
+
+const router = express.Router();
+
+//  GET all items
+router.get("/", async (req, res) => {
+  try {
+    const items = await itemModel.find({ name: { $exists: true } });
+
+    if (items.length === 0) {
+      return res.status(404).json({ message: "No fruits found" });
+    }
+
+    res.status(200).json({ data: items });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+});
+
+//  GET single item by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const item = await itemModel.findById(req.params.id);
+    if (!item) return res.status(404).json({ message: "Item not found" });
+
+    res.status(200).json({ data: item });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+});
+
+export default router;
