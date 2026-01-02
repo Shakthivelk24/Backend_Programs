b26700aacf44bc301bd9b2b8a2133d3d80a91a16
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-02 21:26:44 +0530

    Database

---
+++ SemPrograms/E-Commerce/db.mjs
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const connetDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://<username>:<password>@cluster0.xxxxxx.mongodb.net/"
+    ); //Replace with your own connection string
+    console.log("conneted to Database.....");
+  } catch (error) {
+    console.log(error);
+  }
+};
+export default connetDB;
