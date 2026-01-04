b3a28dae7e9f00db958f5661c5cd9aa0cfd761b4
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-01-04 22:45:48 +0530

    Router for register,login,POST,PUT,DELETE

---
+++ SemPrograms/Medical appointment/adminRouter.mjs
@@ -0,0 +1,137 @@
+// Admin router handling registration, login, and item management with role-based access control
+import express from "express";
+import medicalModel from "./medicalModel.mjs";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import authorize from "./newAuth.mjs";
+
+const router = express.Router();
+// Register and Login routes is same for all type of the Questions asked in the Semester
+// Register
+router.post("/register", async (req, res) => {
+  const { userName, password, role } = req.body;
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new medicalModel({
+      userName,
+      password: hashPassword,
+      role
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "Registration successful..." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.post("/patient", async (req, res) => {
+  const { patientName, password } = req.body;
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+    const roles = "patient";
+
+    const newUser = new medicalModel({
+      patientName,
+      password: hashPassword,
+      role: roles
+    });
+
+    await newUser.save();
+    res.status(201).json({ message: "Registration successful..." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+
+// Login
+router.post("/login", async (req, res) => {
+  const { userName, password } = req.body;
+
+  try {
+    const user = await medicalModel.findOne({ userName });
+
+    if (!user) {
+      return res.status(401).json({ message: "user not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.json({ message: "invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role }, //payload for token user id and role
+      "shakthivelk",
+      { expiresIn: "20m" }
+    );
+
+    res.json({ token });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+// End of Register and Login routes
+
+//  ADD new item by admin only
+
+router.post(
+  "/",
+  authorize(["patient"]),  // Protect this route for 'admin' role only
+  async (req, res) => {
+    const {patientName,doctorName,specialization,appointmentDate} = req.body;// destructuring the req body according to the schema
+    try {
+    const newItem = new medicalModel({ 
+      patientName,                                // change the fields as per requirement given in the question
+      doctorName,
+      specialization,
+      appointmentDate,
+      notes
+    });
+    await newItem.save();   
+    res.status(201).json({ message: "New record added", data: newItem });
+    }
+    catch (error) {
+      res.status(500).json({ message: "Server error", error: error.message });
+    }    
+  }
+);
+
+
+//  UPDATE item by admin only
+router.put("/:id",authorize(["admin"]), async (req, res) => {
+  try {
+    const item = await medicalModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!item) return res.status(404).json({ message: "Item not found" });
+
+    res.status(200).json({ message: "Updated successfully", data: item });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+});
+
+//  DELETE item by admin only
+router.delete("/:id", authorize(["admin"]), async (req, res) => {
+  try {
+    const item = await medicalModel.findByIdAndDelete(req.params.id);
+    if (!item) return res.status(404).json({ message: "Item not found" });
+
+    res.status(200).json({ message: "Deleted successfully", data: item });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+});
+
+export default router;
